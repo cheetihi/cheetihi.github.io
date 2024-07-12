4327710ad1fd38a9812ae722996e6bd30dfa40f9
cheetihi <168654422+cheetihi@users.noreply.github.com>
2024-07-12 07:48:48 -0400

    chane-js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Software Engineer" , "Data Analyst" , "Frontend Developer"],
+    strings: ["Software Engineer" , "Full-stack Developer" , "Frontend Developer" , "Web developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
